5576a5b8396e4b3f7550bc9cdff3f41409fa58b6
NadavKeren <nadav.keren@gmail.com>
2025-12-24 04:34:10 +0200

    Copied the aliases to the same directory as the containerfile

---
+++ containers/test-container/aliases.sh
@@ -0,0 +1,2 @@
+alias download_metakv2="wget -O metakv2_all_sort.csv.zst https://cache-datasets.s3.amazonaws.com/cache_dataset_txt/2022_metaKV/202210_kv_traces_all_sort.csv.zst"
+alias download_metakv4="wget -O metakv4_all_sort.csv.zst https://cache-datasets.s3.amazonaws.com/cache_dataset_txt/2022_metaKV/202401_kv_traces_all_sort.csv.zst"
